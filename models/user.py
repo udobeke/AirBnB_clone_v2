ae44316cc4289d16d3a8423ceeae744fbd1ce90a
Godspower <harshmallins@gmail.com>
2023-03-22 10:14:37 -0700

    AirBnB CV2

--- models/user.py
+++ models/user.py
@@ -1,25 +1,19 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel, Base
-from models import storage_type
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+"""Defines the User class."""
+from models.base_model import BaseModel
 
 
-class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
-    __tablename__ = 'users'
-    if storage_type == 'db':
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship('Place', backref='user',
-                              cascade='all, delete, delete-orphan')
-        reviews = relationship('Review', backref='user',
-                               cascade='all, delete, delete-orphan')
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+class User(BaseModel):
+    """Represent a User.
+
+    Attributes:
+        email (str): The email of the user.
+        password (str): The password of the user.
+        first_name (str): The first name of the user.
+        last_name (str): The last name of the user.
+    """
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
