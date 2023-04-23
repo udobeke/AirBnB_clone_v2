01b40797cd38b16130f3f09c53c4b7fea36cbce8
root <pchinagwam@gmail.com>
2023-04-23 19:32:03 +0000

    tasks

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""A script that starts a Flask web application:
-    The web application must be listening on 0.0.0.0, port 5000"""
+"""
+A script that starts a Flask web application:
+    The web application must be listening on 0.0.0.0, port 5000
+"""
 
 
 from flask import Flask
