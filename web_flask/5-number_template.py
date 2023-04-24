ea6db322b186c38667c8fce14d024041cb3e340c
Peace Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-04-24 11:05:29 +0100

    Update 5-number_template.py

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,54 +1,33 @@
 #!/usr/bin/python3
-"""
-a script that starts a Flask web application:
-
-Your web application must be listening on 0.0.0.0, port 5000
-Routes:
-/: display “Hello HBNB!”
-/hbnb: display “HBNB”
-/c/<text>: display “C ”, followed by the value of the text variable (replace underscore _ symbols with a space )
-/python/(<text>): display “Python ”, followed by the value of the text variable (replace underscore _ symbols with a space )
-The default value of text is “is cool”
-/number/<n>: display “n is a number” only if n is an integer
-/number_template/<n>: display a HTML page only if n is an integer:
-H1 tag: “Number: n” inside the tag BODY
-You must use the option strict_slashes=False in your route definition
-"""
-
+"""A script that starts a Flask web application:"""
 
 from flask import Flask, render_template
-
 
 app = Flask('__name__')
 
 
 @app.route('/', strict_slashes=False)
-def index():
-    """Return the Home Page"""
+def hello_hbnb():
+    """Displays "Hello HBNB!"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Return HBNB"""
-    return 'HBNB!'
+    """Displays HBNB"""
+    return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def cisfun(text):
-    """display “C ” followed by the value of the text variable
-    (replace underscore _ symbols with a space )
-    """
+def c(text):
+    """display “C ” followed by the value of the text variable"""
     return 'C {}'.format(text.replace('_', ' '))
 
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def pythoniscool(text='is cool'):
-    """Display “Python ”, followed by the value of the text variable
-    (replace underscore _ symbols with a space )
-    The default value of text is “is cool”
-    """
+    """Display “Python ”, followed by the value of the text variable"""
     return 'Python {}'.format(text.replace('_', ' '))
 
 
