4f16e47782eb7a60809acf611487ddc5f31ec0c9
Peace Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-04-24 12:04:37 +0100

    Update 5-number_template.py

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,39 +1,40 @@
 #!/usr/bin/python3
-"""A script that starts a Flask web application:"""
+"""A script that starts a Flask web application."""
 
 from flask import Flask, render_template
+
 
 app = Flask('__name__', template_folder='templates')
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """Displays "Hello HBNB!"""
+    """Displays "Hello HBNB!."""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Displays HBNB"""
+    """Displays HBNB."""
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
-    """display “C ” followed by the value of the text variable"""
+    """Display “C ” followed by the value of the text variable."""
     return 'C {}'.format(text.replace('_', ' '))
 
 
 @app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python(text):
-    """Display “Python ”, followed by the value of the text variable"""
+    """Display “Python ”, followed by the value of the text variable."""
     return 'Python {}'.format(text.replace('_', ' '))
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
-    """display “n is a number” only if n is an integer"""
+    """Display “n is a number” only if n is an integer."""
     return '{} is a number'.format(n)
 
 
