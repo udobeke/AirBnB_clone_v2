f6fb02c2597d751430eb7f97d720285897131d5c
Peace Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-04-24 12:10:36 +0100

    Update 6-number_odd_or_even.py

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,20 +1,11 @@
 #!/usr/bin/python3
 """
 A script that starts a Flask web application:
-
 Your web application must be listening on 0.0.0.0, port 5000
 Routes:
 /: display “Hello HBNB!”
-/hbnb: display “HBNB”
-/c/<text>: display “C ”, followed by the value of the text variable (replace underscore _ symbols with a space )
-/python/(<text>): display “Python ”, followed by the value of the text variable (replace underscore _ symbols with a space )
-The default value of text is “is cool”
-/number/<n>: display “n is a number” only if n is an integer
-/number_template/<n>: display a HTML page only if n is an integer:
-H1 tag: “Number: n” inside the tag BODY
-/number_odd_or_even/<n>: display a HTML page only if n is an integer:
-H1 tag: “Number: n is even|odd” inside the tag BODY
 """
+
 from flask import Flask, render_template
 
 
@@ -22,50 +13,45 @@ app = Flask('__name__')
 
 
 @app.route('/', strict_slashes=False)
-def index():
-    """Return the Home Page"""
+def hello_hbnb():
+    """Displays "Hello HBNB!"."""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Return HBNB"""
-    return 'HBNB!'
+    """Displays "HBNB"."""
+    return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def cisfun(text):
-    """display “C ” followed by the value of the text variable
-    (replace underscore _ symbols with a space )
-    """
+def c(text):
+    """Route that displays "C " followed by the value of the text variable."""
     return 'C {}'.format(text.replace('_', ' '))
 
 
-@app.route('/python', strict_slashes=False)
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def pythoniscool(text='is cool'):
-    """Display “Python ”, followed by the value of the text variable
-    (replace underscore _ symbols with a space )
-    The default value of text is “is cool”
-    """
+def python(text):
+    """Route display “Python ”, followed by the value of the text variable."""
     return 'Python {}'.format(text.replace('_', ' '))
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def isnumber(n):
-    """display “n is a number” only if n is an integer"""
-    if isinstance(n, int):
-        return '{} is a number'.format(n)
+def number(n):
+    """Route display “n is a number” only if n is an integer."""
+    return '{} is a number'.format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def numbertemplate(n=None):
-    if isinstance(n, int):
-        return render_template('5-number.html', n=n)
+def number_template(n):
+    """Displays a HTML page only if n is an integer H1 tag: "Number: n" inside
+    the tag BODY."""
+    return render_template('5-number.html', n=n))
 
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def numberoddoreven(n=None):
+def numberoddoreven(n):
     if isinstance(n, int):
         if n % 2 == 0:
             number = 'even'
@@ -75,4 +61,4 @@ def numberoddoreven(n=None):
 
 
 if __name__ == "__main__":
-    app.run(host = '0.0.0.0', port = 5000, debug = None)
+    app.run(host = '0.0.0.0', port = 5000)
