f74593b5585e3e6ee1c4bdef4b29387c2a900374
Peace Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-04-24 11:45:35 +0100

    Update 5-number_template.py

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -38,7 +38,7 @@ def number(n):
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n=None):
+def number_template(n):
     """Display a HTML page only if n is an integer."""
     if isinstance(n, int):
         return render_template('5-number.html', n=n)
