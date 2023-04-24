0ed823adc959847c498a293de2f9757037f14c65
Peace Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-04-24 10:41:33 +0100

    Update 3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -8,22 +8,27 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def index():
     """Return the Home Page"""
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """Return HBNB"""
     return 'HBNB'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def cisfun(text):
     """Display “C ” followed by the value of the text variable
     (replace underscore _ symbols with a space )
     """
-    return 'C {}'.format(text.replace('_', ' '))
+   
+   return 'C {}'.format(text.replace('_', ' '))
+
 
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
@@ -36,4 +41,3 @@ def pythoniscool(text='is cool'):
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
-
