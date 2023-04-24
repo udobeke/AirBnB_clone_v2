3855a4d783757f5c8b0c04bd3d9f22cc0370bc74
Peace Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-04-24 10:50:38 +0100

    Update 4-number_route.py

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """
 a script that starts a Flask web application:
 
@@ -6,16 +7,12 @@ Your web application must be listening on 0.0.0.0, port 5000
 Routes:
 /: display “Hello HBNB!”
 /hbnb: display “HBNB”
-/c/<text>: display “C ”, followed by the value of the text variable (replace underscore _ symbols with a space )
-/python/(<text>): display “Python ”, followed by the value of the text variable (replace underscore _ symbols with a space )
 The default value of text is “is cool”
 /number/<n>: display “n is a number” only if n is an integer
 You must use the option strict_slashes=False in your route definition
 """
 
-
 from flask import Flask
-
 
 app = Flask('__name__')
 
@@ -45,7 +42,6 @@ def cisfun(text):
 def pythoniscool(text='is cool'):
     """Display “Python ”, followed by the value of the text variable
     (replace underscore _ symbols with a space )
-    The default value of text is “is cool”
     """
     return 'Python {}'.format(text.replace('_', ' '))
 
@@ -58,4 +54,4 @@ def isnumber(n=None):
 
 
 if __name__ == "__main__":
-    app.run(host = '0.0.0.0', port = 5000, debug = None)
+    app.run(host='0.0.0.0', port=5000, debug=None)
