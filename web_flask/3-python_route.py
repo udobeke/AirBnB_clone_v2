db8b29f7ddaee783c156df4cecfa3538ca05de66
Peace Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-04-24 10:43:57 +0100

    Update 3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
 """
 A script that starts a Flask web application:
+    Your web application must be listening on 0.0.0.0, port 5000
+    Routes:
+        /: display “Hello HBNB!”
+        /hbnb: display “HBNB”
+        /c/<text>: display “C ” followed by the value of the text
+        variable (replace underscore _ symbols with a space )
+        /python/(<text>): display “Python ”, followed by the
+        value of the text variable (replace underscore _ symbols with a space
 """
 
 
 from flask import Flask
 
-app = Flask(__name__)
+
+app = Flask('__name__')
 
 
 @app.route('/', strict_slashes=False)
@@ -22,22 +31,17 @@ def hbnb():
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def cisfun(text):
-    """Display “C ” followed by the value of the text variable
-    (replace underscore _ symbols with a space )
-    """
-   
-   return 'C {}'.format(text.replace('_', ' '))
+def cisfun(text=None):
+    """display “C ” followed by the value of the text variable"""
+    return 'C {}'.format(text.replace('_', ' '))
 
 
-@app.route('/python/', strict_slashes=False)
+@app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def pythoniscool(text='is cool'):
-    """Display “Python ”, followed by the value of the text variable
-    (replace underscore _ symbols with a space )
-    The default value of text is “is cool”
-    """
+    """Display “Python ”, followed by the value of the text variable"""
     return 'Python {}'.format(text.replace('_', ' '))
 
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=None)
